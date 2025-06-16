221f58bc4a44d49c09781d953ccf7976d5783740
iam-vihaan <asathyavihaan@gmail.com>
2025-06-16 17:41:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
 
 module "ec2_instance" {
   source        = "./modules/ec2_instance"
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  name          = "MyEC2Instance"
+  ami           = var.ami
+  instance_type = var.instance_type
+  name          = var.ec2_name
+  region        = var.region
 }
 
-module "sns_topic" {
-  source = "./modules/sns_topic"
-  name   = "ec2-state-change-topic"
+module "sns" {
+  source = "./modules/sns"
+  name   = var.sns_topic_name
 }
 
 module "cloudwatch_alarm" {
   source         = "./modules/cloudwatch_alarm"
-  alarm_name     = "EC2StateChangeAlarm"
+  alarm_name     = var.alarm_name
   instance_id    = module.ec2_instance.instance_id
   sns_topic_arn  = module.sns_topic.topic_arn
 }
