f4c132b7cf56188ac195baa4e1644739dfd07573
iam-vihaan <asathyavihaan@gmail.com>
2025-06-16 17:48:12 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "The IDs of the public subnets"
+  value       = module.vpc.public_subnet_ids
+}
+
 output "ec2_instance_id" {
   description = "ID of the EC2 instance"
   value       = module.ec2_instance.instance_id
