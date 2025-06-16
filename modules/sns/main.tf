8035d15e41310761228458f8c266bb71a0eedf3b
iam-vihaan <asathyavihaan@gmail.com>
2025-06-16 17:20:02 +0530

    Update main.tf

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -1,3 +1,41 @@
-resource "aws_sns_topic" "ec2-state-change-topic" {
-  name = var.name
+# SNS module for notifications
+
+resource "aws_sns_topic" "notifications" {
+  name = var.topic_name
+
+  tags = {
+    Name        = var.topic_name
+    Environment = var.environment
+  }
+}
+
+# Create email subscriptions with confirmation disabled
+resource "aws_sns_topic_subscription" "email_subscriptions" {
+  count                  = length(var.email_subscriptions)
+  topic_arn              = aws_sns_topic.notifications.arn
+  protocol               = "email"
+  endpoint               = var.email_subscriptions[count.index]
+  endpoint_auto_confirms = true
+
+  depends_on = [aws_sns_topic.notifications]
+}
+
+# SNS topic policy
+resource "aws_sns_topic_policy" "default" {
+  arn = aws_sns_topic.notifications.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowLambdaPublish"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action   = "SNS:Publish"
+        Resource = aws_sns_topic.notifications.arn
+      }
+    ]
+  })
 }
