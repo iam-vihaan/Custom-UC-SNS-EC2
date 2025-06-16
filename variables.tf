c25c1fcb2df81dd15fd869b1d9ad4057fb527f8e
iam-vihaan <asathyavihaan@gmail.com>
2025-06-16 17:49:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "region" {
   description = "AWS region to deploy resources"
   type        = string
@@ -32,4 +38,22 @@ variable "alarm_name" {
   description = "Name of the CloudWatch alarm"
   type        = string
   default     = "EC2StateChangeAlarm"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for the public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones for the subnets"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
 }
