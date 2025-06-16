6ef9790d0b26df5423b0dec6275176677b060213
iam-vihaan <asathyavihaan@gmail.com>
2025-06-16 17:22:18 +0530

    Update output.tf

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,3 +1,7 @@
-output "instance_id" {
-  value = aws_instance.this.id
+output "web_instance_ids" {
+  value = aws_instance.web[*].id
+}
+
+output "web_instance_ips" {
+  value = aws_instance.web[*].public_ip
 }
